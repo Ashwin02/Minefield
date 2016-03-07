64e0620afb8e21ee4706a7f7e5a1de28f94e7767
Ashwin02 <ashwin.s.patil10@gmail.com>
2016-03-07 11:01:33 -0800

    Tested Minfield Project

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "Field.h"
- 
+
 class FieldTest : public ::testing::Test
 {
 	protected:
@@ -18,7 +18,20 @@ class FieldTest : public ::testing::Test
 TEST(FieldTest, placeMineInBounds)
 {
 	Field minefield;
-	
+
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+TEST (FieldTest, minePresent)
+{
+  Field minefield;
+  minefield.placeMine(5,5);
+  ASSERT_TRUE(minefield.isSafe(5,5));
+}
+
+TEST (FieldTest, mineAbsent)
+{
+  Field minefield;
+  minefield.placeMine(5,5);
+  ASSERT_FALSE(minefield.isSafe(4,5));
 }
